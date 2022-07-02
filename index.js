5dde04ae72753fdba576099c62496e9a8af36df8
Ianis Mauraisin <tsaitrema@hotmail.fr>
2022-07-02 15:25:10 +0200

    cnotserver

---
+++ index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const cors = require("cors");
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// const uri = "mongodb://localhost:27017/dbMern";
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connexion à la BDD établie"))
+  .catch((error) => console.log(error));
+
+const userRoute = require("./routes/userRoute");
+app.use("/user", userRoute);
+
+const noteRoute = require("./routes/noteRoute");
+app.use("/note", noteRoute);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Le serveur est lancé sur le port ${PORT}`));
---
+++ routes/noteRoute.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const router = express.Router();
+const Note = require("../models/Note");
+const auth = require("../middleware/auth");
+
+//// Création d'une note :
+router.post("/", auth, async (req, res) => {
+  try {
+    req.body.idUser = req.payload.id;
+    const note = new Note(req.body);
+
+    const newNote = await note.save();
+    res.status(201).json(newNote);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
+//// Lister toutes les notes ( on peut les trier avec .sort())
+router.get("/", async (req, res) => {
+  try {
+    const listeNotes = await Note.find().sort("createdAt");
+    res.json(listeNotes);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
+//// Récupérer les notes du user connecté
+router.get("/getNoteUser", auth, async (req, res) => {
+  try {
+    const notes = await Note.find({ idUser: req.payload.id });
+
+    res.json(notes);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
+//// Récupérer une note avec son id
+router.get("/:id", async (req, res) => {
+  try {
+    const note = await Note.findById(req.params.id);
+
+    if (!note) {
+      return res.status(404).json("Cette note n'existe pas");
+    }
+    res.json(note);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
+//// Supprimer une note (si elle existe)
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const note = await Note.findById(req.params.id);
+
+    if (!note) {
+      return res.status(404).json("Cette note n'existe pas");
+    }
+    /// on ne peut pas mettre "!==" car ils n'ont pas le même type:
+    if (note.idUser != req.payload.id) {
+      return res.status(401).json("Cette note ne vous appartient pas");
+    }
+    await note.remove();
+    res.status(200).json("note supprimée");
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
+//// Mettre à jour une note
+router.put("/:id", auth, async (req, res) => {
+  try {
+    const note = await Note.findById(req.params.id);
+    if (!note) {
+      return res.status(404).json("Cette note n'existe pas");
+    }
+    if (note.idUser != req.payload.id) {
+      return res.status(401).json("Cette note ne vous appartient pas");
+    }
+
+    note.nom = req.body.nom;
+    note.prix = req.body.prix;
+    note.description = req.body.description;
+    note.photo = req.body.photo;
+    note.qteDispo = req.body.qteDispo;
+
+    await note.save();
+    res.json(note);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
+module.exports = router;
